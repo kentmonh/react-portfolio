22723f28b653fe1d51def2e8caa5636ae5e73139
Minh Nguyen <mnguyen2606@gmail.com>
2020-12-17 05:24:56 +0700

    Enhanced the view of Project section.

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,33 +2,38 @@ import React from "react"
 import '../style/components/Project.scss';
 
 
-
 function Item(props) {
   return (
-    <div class="row m-5">
-      <div className={`col-lg-6 ${props.index}`}>
-        <img src={require(`../images/${props.image}.png`).default} class="img-fluid" alt={props.image} />
+    <div class="row">
+      <div className={`col-lg-6 ${props.background} ${props.index}`}>
+        <div class="m-5">
+          <img src={require(`../images/${props.image}.png`).default} class="img-fluid" alt={props.image} />
+        </div>
+
       </div>
 
-      <div class="col-lg-6">
-        <h2 class="title-text-on-black mb-5">{props.title}</h2>
-        <p class="content-text-on-black">
-          {props.content1} <br />
-          {props.content2}
-        </p>
-        <p class="content-text-on-black mt-5">
-          Techologies used: <br />
-          {props.technology}
-        </p>
+      <div className={`col-lg-6 ${props.background}`}>
+        <div class="m-5">
+          <h2 className={`mb-5 ${props.titleText}`}>{props.title}</h2>
+          <p className={`${props.contentText}`}>
+            {props.content1} <br />
+            {props.content2}
+          </p>
+          <p className={`mt-5 ${props.contentText}`}>
+            Techologies used: <br />
+            {props.technology}
+          </p>
 
-        <div class="row">
-          <div class="m-3">
-            <a href="https://github.com/kentmonh/ecommer" target="_blank" rel="noopener noreferrer" class="btn btn-outline-warning" role="button" aria-pressed="true">Github</a>
-          </div>
-          <div class="m-3">
-            <a href="https://fragrance-ecommer.herokuapp.com/" target="_blank" rel="noopener noreferrer" class="btn btn-outline-warning" role="button" aria-pressed="true">Live Preview</a>
+          <div class="row">
+            <div class="m-3">
+              <a className={`btn ${props.button}`} href="https://github.com/kentmonh/ecommer" target="_blank" rel="noopener noreferrer" role="button" aria-pressed="true">Github</a>
+            </div>
+            <div class="m-3">
+              <a className={`btn ${props.button}`} href="https://fragrance-ecommer.herokuapp.com/" target="_blank" rel="noopener noreferrer" role="button" aria-pressed="true">Live Preview</a>
+            </div>
           </div>
         </div>
+
       </div>
 
     </div>
@@ -40,11 +45,27 @@ function Project(props) {
     <div id="project">
       <div class="Project">
 
-        <div class="m-5">
+        <div class="mt-5">
           <h2 class="title-text-on-black">Projects</h2>
         </div>
 
-        <Item title="Ecommer Fragrance Site" index="order-lg-2" image="project1"
+        <Item background="background-black" 
+          index="order-lg-2" 
+          image="project1" 
+          titleText="title-text-on-black"
+          contentText="content-text-on-black"
+          button="btn-outline-warning"
+          title="Ecommer Fragrance Site"
+          content1="Developed an full features ecommerce website from scratch using Ruby on Rails."
+          content2="The functions of website are using devise to manage admin and back end stuff, users register, search products, create the cart for every customer, create and pay order using Stripe."
+          technology="Ruby on Rails, HTML, Bootstrap, Stripe" />
+
+        <Item background="background-yellow"
+          image="project1" 
+          titleText="title-text-on-white"
+          contentText="content-text-on-white"
+          button="btn-outline-dark"
+          title="Ecommer Fragrance Site"  
           content1="Developed an full features ecommerce website from scratch using Ruby on Rails."
           content2="The functions of website are using devise to manage admin and back end stuff, users register, search products, create the cart for every customer, create and pay order using Stripe."
           technology="Ruby on Rails, HTML, Bootstrap, Stripe" />
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css';
 import "typeface-lato";
 import "typeface-open-sans";
@@ -13,6 +14,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Experience from './components/Experience';
 import Project from './components/Project';
+
 
 ReactDOM.render(
   <React.StrictMode>
