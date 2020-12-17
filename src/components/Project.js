c9f22b7bd47531e2262c268f7091ede88ab3d396
Minh Nguyen <mnguyen2606@gmail.com>
2020-12-17 23:11:42 +0700

    Updated Project section.

--- src/components/Project.js
+++ src/components/Project.js
@@ -26,10 +26,10 @@ function Item(props) {
 
           <div class="row">
             <div class="m-3">
-              <a className={`btn ${props.button}`} href="https://github.com/kentmonh/ecommer" target="_blank" rel="noopener noreferrer" role="button" aria-pressed="true">Github</a>
+              <a className={`btn ${props.button}`} href={props.github} target="_blank" rel="noopener noreferrer" role="button" aria-pressed="true">Github</a>
             </div>
             <div class="m-3">
-              <a className={`btn ${props.button}`} href="https://fragrance-ecommer.herokuapp.com/" target="_blank" rel="noopener noreferrer" role="button" aria-pressed="true">Live Preview</a>
+              <a className={`btn ${props.button}`} href={props.live} target="_blank" rel="noopener noreferrer" role="button" aria-pressed="true">Live Preview</a>
             </div>
           </div>
         </div>
@@ -58,17 +58,20 @@ function Project(props) {
           title="Ecommer Fragrance Site"
           content1="Developed an full features ecommerce website from scratch using Ruby on Rails."
           content2="The functions of website are using devise to manage admin and back end stuff, users register, search products, create the cart for every customer, create and pay order using Stripe."
-          technology="Ruby on Rails, HTML, Bootstrap, Stripe" />
+          technology="Ruby on Rails, HTML, Bootstrap, Stripe"
+          github="https://github.com/kentmonh/ecommer"
+          live="https://fragrance-ecommer.herokuapp.com/" />
 
         <Item background="background-yellow"
-          image="project1" 
+          image="project2"
           titleText="title-text-on-white"
           contentText="content-text-on-white"
           button="btn-outline-dark"
-          title="Ecommer Fragrance Site"  
-          content1="Developed an full features ecommerce website from scratch using Ruby on Rails."
-          content2="The functions of website are using devise to manage admin and back end stuff, users register, search products, create the cart for every customer, create and pay order using Stripe."
-          technology="Ruby on Rails, HTML, Bootstrap, Stripe" />
+          title="SOS Application"  
+          content1="Built an android application using Java, the app is a hub for people need help and rescue teams in emergency situations."
+          content2="The functions of are sign in by Google account, mark locations for sos, rescue team or safe spaces, and users who created can delete markers."
+          technology="Java, Firebase Realtime, Mapbox"
+          github="https://github.com/kentmonh/sos-project-android" />
 
       </div>
     </div>
